22b933677bbf0d79762596b926ef32338bb9a6bb
Stanislav Sazonov <slavk11@yandex.ru>
2024-06-27 17:16:43 +0300

    Completed base functiions Refactoring

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
@@ -12,6 +13,49 @@ import (
 var gBot *tgbotapi.BotAPI
 var gToken string
 var gChatID int64
+
+var gUsersInChat Users
+var gUsefulActivities = Activities{
+	// Саморазвитие
+	{"yoga", "Yoga (15 minutes)", 1},
+	{"meditation", "Meditation (15 minutes)", 1},
+	{"language", "Learning a foreign language (15 minutes)", 1},
+	{"swimming", "Swimming (15 minutes)", 1},
+	{"walk", "Walk (15 minutes)", 1},
+	{"chores", "Chores", 1},
+
+	// Work
+	{"work_learning", "Studying work materials (15 minutes)", 1},
+	{"portfolio_work", "Working on a portfolio project (15 minutes)", 1},
+	{"resume_edit", "Resume editing (15 minutes)", 1},
+
+	// Creativity
+	{"creative", "Creative creation (15 minutes)", 1},
+	{"reading", "Reading fiction literature (15 minutes)", 1},
+}
+
+var gRewards = Activities{
+	// Entertainment
+	{"watch_series", "Watching a series (1 episode)", 10},
+	{"watch_movie", "Watching a movie (1 item)", 30},
+	{"social_nets", "Browsing social networks (30 minutes)", 10},
+
+	// Food
+	{"eat_sweets", "300 kcal of sweets", 60},
+}
+
+type User struct {
+	id    int64
+	name  string
+	coins uint16
+}
+type Users []*User
+
+type Activity struct {
+	code, name string
+	coins      uint16
+}
+type Activities []*Activity
 
 func init() {
 	//_ = os.Setenv(TOKEN_NAME_IN_OS, "INSERT_YOUR_TOKEN")
@@ -33,7 +77,7 @@ func isStartMessage(update *tgbotapi.Update) bool {
 	return update.Message != nil && update.Message.Text == "/start"
 }
 
-func isCallbackQuey(update *tgbotapi.Update) bool {
+func isCallbackQuery(update *tgbotapi.Update) bool {
 	return update.CallbackQuery != nil && update.CallbackQuery.Data != ""
 }
 
@@ -61,6 +105,9 @@ func printIntro(update *tgbotapi.Update) {
 func getKeyboardRow(buttonText, buttonCode string) []tgbotapi.InlineKeyboardButton {
 	return tgbotapi.NewInlineKeyboardRow(tgbotapi.NewInlineKeyboardButtonData(buttonText, buttonCode))
 }
+func sendStringMessage(msg string) {
+	gBot.Send(tgbotapi.NewMessage(gChatID, msg))
+}
 
 func askToPrintIntro() {
 	msg := tgbotapi.NewMessage(gChatID, "Во вступительных сообщениях ты можешь найти смысл данного бота, и правила игры. Что думаешь?")
@@ -70,7 +117,7 @@ func askToPrintIntro() {
 	)
 	gBot.Send(msg)
 }
-func showMenu(update *tgbotapi.Update) {
+func showMenu() {
 	msg := tgbotapi.NewMessage(gChatID, "Выбери один из вариантов:")
 	msg.ReplyMarkup = tgbotapi.NewInlineKeyboardMarkup(
 		getKeyboardRow(BUTTON_TEXT_BALANCE, BUTTON_CODE_BALANCE),
@@ -79,15 +126,151 @@ func showMenu(update *tgbotapi.Update) {
 	)
 	gBot.Send(msg)
 }
+func showBalance(user *User) {
+	msg := fmt.Sprintf("%s, твой кошелёк пока пуст %s \nЗатрекай полезное действие, чтобы получить монеты", user.name, EMOJI_DONT_KNOW)
+	if coins := user.coins; coins > 0 {
+		msg = fmt.Sprintf("%s, you have %d %s", user.name, coins, EMOJI_COIN)
+	}
+	gBot.Send(tgbotapi.NewMessage(gChatID, msg))
+	//sendStringMessage(msg)
+	showMenu()
+
+}
+
+func callbackQueryIsMissing(update *tgbotapi.Update) bool {
+	return update.CallbackQuery == nil || update.CallbackQuery.From == nil
+}
+
+func getUserFromUpdate(update *tgbotapi.Update) (user *User, found bool) {
+	if callbackQueryIsMissing(update) {
+		return
+	}
+	userID := update.CallbackQuery.From.ID
+	for _, userInChat := range gUsersInChat {
+		if userID == userInChat.id {
+			return userInChat, true
+		}
+	}
+	return
+}
+
+func storeUserFromUpdate(update *tgbotapi.Update) (user *User, found bool) {
+	if callbackQueryIsMissing(update) {
+		return
+	}
+	from := update.CallbackQuery.From
+	user = &User{id: from.ID, name: strings.TrimSpace(from.FirstName + " " + from.LastName), coins: 0}
+	gUsersInChat = append(gUsersInChat, user)
+	return user, true
+}
+func showActivities(activities Activities, message string, isUseful bool) {
+	activitiesButtonsRows := make([]([]tgbotapi.InlineKeyboardButton), 0, len(gUsefulActivities)+1)
+	for _, activity := range activities {
+		activityDescription := ""
+		if isUseful {
+			activityDescription = fmt.Sprintf("+ %d %s: %s", activity.coins, EMOJI_COIN, activity.name)
+		} else {
+			activityDescription = fmt.Sprintf("- %d %s", activity.coins, EMOJI_COIN, activity.name)
+		}
+		activitiesButtonsRows = append(activitiesButtonsRows, getKeyboardRow(activityDescription, activity.code))
+	}
+	activitiesButtonsRows = append(activitiesButtonsRows, getKeyboardRow(BUTTON_TEXT_PRINT_MENU, BUTTON_CODE_PRINT_MENU))
+
+	msg := tgbotapi.NewMessage(gChatID, message)
+	msg.ReplyMarkup = tgbotapi.NewInlineKeyboardMarkup(activitiesButtonsRows...)
+	gBot.Send(msg)
+}
+func showUsefulActivities() {
+	showActivities(gUsefulActivities, "Трекай полезное действие или возвращайся в главное меню:", false)
+}
+
+func showRewards() {
+	showActivities(gRewards, "Купи вознаграждение или возвращайся в главное меню:", false)
+}
+func findActivity(activities Activities, choiceCode string) (activity *Activity, found bool) {
+	for _, activity := range activities {
+		if choiceCode == activity.code {
+			return activity, true
+		}
+	}
+	return
+}
+
+func processUsefulActivity(activity *Activity, user *User) {
+	errorMsg := ""
+	if activity.coins == 0 {
+		errorMsg = fmt.Sprintf(`у активности "%s" не указана стоимость`, activity.name)
+	} else if user.coins+activity.coins > MAX_USER_COINS {
+		errorMsg = fmt.Sprintf("у тебя не может быть больше %d %s", MAX_USER_COINS, EMOJI_COIN)
+	}
+
+	resultMessage := ""
+	if errorMsg != "" {
+		resultMessage = fmt.Sprintf("%s, прости, но %s %s Твой баланс остался без изменений.", user.name, errorMsg, EMOJI_SAD)
+	} else {
+		user.coins += activity.coins
+	}
+	gBot.Send(tgbotapi.NewMessage(gChatID, resultMessage))
+}
+
+func processReward(activity *Activity, user *User) {
+	errorMsg := ""
+	if activity.coins == 0 {
+		errorMsg = fmt.Sprintf(`у вознаграждения "%s" не указана стоимость`, activity.name)
+	} else if user.coins < activity.coins {
+		errorMsg = fmt.Sprintf(`у тебя сейчас  %d %s. ты на можешь себе позволить "%s" за %d %s`, user.coins, EMOJI_COIN, activity.name, activity.coins, EMOJI_COIN)
+	}
+
+	resultMessage := ""
+	if errorMsg != "" {
+		resultMessage = fmt.Sprintf("%s, извиняюсь, но %s %s Твой баланс остался без изменений, вознаграждение не доступно %s", user.name, errorMsg, EMOJI_SAD, EMOJI_DONT_KNOW)
+	} else {
+		user.coins -= activity.coins
+		resultMessage = fmt.Sprintf(`%s, вознаграждение "%s" оплачено, приступай! %d %s было сеяир с ивренр счёта. Теперь у тебя %d %s`, user.name, activity.name, activity.coins, EMOJI_COIN, user.coins, EMOJI_COIN)
+	}
+	gBot.Send(tgbotapi.NewMessage(gChatID, resultMessage))
+}
+
 func updateProcessing(update *tgbotapi.Update) {
+	user, found := getUserFromUpdate(update)
+	if !found {
+		if user, found = storeUserFromUpdate(update); !found {
+			gBot.Send(tgbotapi.NewMessage(gChatID, "Не получается идентифицировать пользователя"))
+			return
+		}
+	}
+
 	choiceCode := update.CallbackQuery.Data
 	log.Println("[%T] %s", time.Now(), choiceCode)
 	switch choiceCode {
-	case BUTTON_CODE_PRINT_INTRO:
+	case BUTTON_CODE_BALANCE:
+		showBalance(user)
+	case BUTTON_CODE_USEFUL_ACTIVITIES:
+		showUsefulActivities()
+	case BUTTON_CODE_REWARDS:
 		printIntro(update)
-		showMenu(update)
+		showRewards()
 	case BUTTON_CODE_SKIP_INTRO:
-		showMenu(update)
+		showMenu()
+	case BUTTON_CODE_PRINT_MENU:
+		showMenu()
+	default:
+		if usefulActivity, found := findActivity(gUsefulActivities, choiceCode); found {
+			processUsefulActivity(usefulActivity, user)
+			delay(2)
+
+			showUsefulActivities()
+			return
+		}
+		if reward, found := findActivity(gRewards, choiceCode); found {
+			processReward(reward, user)
+			delay(2)
+			showRewards()
+			return
+		}
+		log.Printf(`[%T] !!!!!!!!! ERROR: Unknown code "%s"`, time.Now(), choiceCode)
+		msg := fmt.Sprintf("%s, I'm sorry, I don't recognize code '%s' %s Please report this error to my creator.", user.name, choiceCode, EMOJI_SAD)
+		gBot.Send(tgbotapi.NewMessage(gChatID, msg))
 	}
 }
 
@@ -98,14 +281,12 @@ func main() {
 	updateConfig.Timeout = UPDATE_CONFIG_TIMEOUT
 
 	for update := range gBot.GetUpdatesChan(updateConfig) {
-		if isCallbackQuey(&update) {
+		if isCallbackQuery(&update) {
 			updateProcessing(&update)
 		} else if isStartMessage(&update) {
 			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 			gChatID = update.Message.Chat.ID
 			askToPrintIntro()
 		}
-
 	}
-
 }
